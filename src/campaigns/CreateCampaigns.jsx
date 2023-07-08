7e39965f44f48236a800fe5ef2b9bed40982456b
Shanti Bhushan Singh <shantibhushan107@gmail.com>
2023-07-08 20:43:27 +0530

    new commit

--- src/campaigns/CreateCampaigns.jsx
+++ src/campaigns/CreateCampaigns.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import { sendCampaignEmail } from '../service/campaign';
 import { Box, InputBase, Typography, TextField, Button, styled } from '@mui/material';
 import { Close, DeleteOutline } from '@mui/icons-material';
@@ -10,6 +11,7 @@ const Header = styled(Box)({
   justifyContent: "space-between",
   padding: "10px 15px",
   background: "#f2f6fc",
+  borderRadius: 5,
   '& > p': {
     fontSize: 14,
     fontWeight: 500
@@ -50,38 +52,55 @@ const initialValuesEmail = {
   subject: "",
   message: ""
 }
+
+const modules = {
+  toolbar:[
+    [{header: [1,2,3,4,5,6,false]}],
+    [{font: []}],
+    [{size: []}],
+    ["bold", "italic","underline","strike","blockquote"],
+    [
+      {list:"ordered"},
+      {list: "bullet"},
+      {indent:"-1"},
+      {indent:"+1"}
+    ],
+    ["link","image","video"]
+  ]
+}
 export default function CreateCampaigns() {
 
-  const [emailData, setEmailData] = useState(initialValuesEmail)
+  const [emailData, setEmailData] = useState("");
+  const [value, setValue] = useState('');
 
-  const handleChange = ({ target: { name, value } }) => {
-    setEmailData({ ...emailData, [name]: value })
+  const handleChange = ({ target: {value } }) => {
+    setEmailData(value)
+    console.log(emailData)
   }
 
   const sendEmail = async () => {
-    const response = await sendCampaignEmail(emailData)
+    const data = {
+      type: "EMAIL",
+      subject:emailData,
+      message:value
+    }
+    console.log(data)
+    const response = await sendCampaignEmail(data)
     console.log(response)
   }
+
+ 
 
 
   return (
     <Box style={{ display: "flex", height: "100vh", width: "100%", alignItems: "center", justifyContent: "center" }}>
-      <Box style={{ display: "flex", flexDirection: "column", width: "80%", height: " 70%", border: "1px solid black", marginTop: 77 }}>
-        <Header>
-          <Typography>New Message</Typography>
-          <Close fontSize='small' />
-        </Header>
-        <RecipientsWrapper>
+      <Box style={{ display: "flex", flexDirection: "column", width: "80%", height: " 70%", border: "1px solid black", marginTop: 77, borderRadius: 5 }}>
+        <Header >
+        <InputBase placeholder='Subject'  onChange={handleChange} />
           
-          <InputBase placeholder='subject' name="subject" onChange={handleChange} />
-        </RecipientsWrapper>
-        <TextField
-          multiline
-          rows={14}
-          sx={{ '& .MuiOutlinedInput-notchedOutline': { border: 'none' } }}
-          placeholder='message' name="message" onChange={handleChange}
-        />
-        <Footer>
+        </Header>
+        <ReactQuill theme="snow" value={value}  onChange={setValue} modules={modules}/>
+        <Footer style={{marginTop:290}}>
           <SendButton onClick={sendEmail}>Send</SendButton>
           <DeleteOutline />
         </Footer>
